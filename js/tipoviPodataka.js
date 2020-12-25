26b4b15a507dc023e3ac1077c0ff990f3f4bbcbc
Vahid Zekic <vahidzekic@gmail.com>
2020-12-25 17:43:52 +0100

    Update tipoviPodataka.js

--- js/tipoviPodataka.js
+++ js/tipoviPodataka.js
@@ -1,5 +1,5 @@
-var broj = 2020;
-var string = "Novi Pazar"
+var broj1 = 2020;
+var string1 = "Novi Pazar"
 var boolean = true; // or false
 var lista = ["prvi", 2, "TRECI", 4, 5, true]
 var objekat = { ime: "Vahid", prezime: "Zekic", godina: 34, boja: "Crna"};
@@ -9,7 +9,7 @@ var niz = ["Rama", "Kavaz", "Rodjeni", "Waha"];
 function tipoviPodatka() {
     document.getElementById("0").innerHTML = "<b>JavaScript -<mark> TIPOVI PODATAKA</mark><b>"
     document.getElementById("1").innerHTML = niz[2];
-    document.getElementById("2").innerHTML = broj + " " + string + " " + boolean;
+    document.getElementById("2").innerHTML = broj1 + " " + string1 + " " + boolean;
     document.getElementById("3").innerHTML = lista;
     document.getElementById("4").innerHTML = " ";
     document.getElementById("5").innerHTML = " ";
