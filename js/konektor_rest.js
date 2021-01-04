ac0a30877e5401e967c8fa76dab45979e71da571
Vahid Zekic <vahidzekic@gmail.com>
2021-01-04 17:39:00 +0100

    REST API - GET metoda
    Obnavljali smo GET metodu, kako citati podatke iz JSON oblika lokalno i. kako sa serverom.

--- js/konektor_rest.js
+++ js/konektor_rest.js
@@ -1,25 +1,103 @@
-// REST API konekcija GET
-async function restApiGet() {
-    const zahtev = await fetch('http://localhost:5000/get');
-    const odgovor = await zahtev.json();
-
-    var ink = Object.keys(odgovor).length;
-
-    for (const clanovi in odgovor)
+objekatJson = [
     {
-        var clan = odgovor[clanovi].ime + " " + odgovor[clanovi].prezime;
-        document.getElementById(clanovi).innerHTML = clan;
-        console.log(clanovi + ". " + clan);
+        "godine": 36,
+        "id": 3,
+        "ime": "Rama",
+        "pol": "muski",
+        "prezime": "Zekic"
+    },
+    {
+        "godine": 36,
+        "id": 3,
+        "ime": "Jale",
+        "pol": "muski",
+        "prezime": "Jasko"
+    },
+    {
+        "godine": 36,
+        "id": 3,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
+    },
+    {
+        "godine": 36,
+        "id": 4,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
+    },
+    {
+        "godine": 36,
+        "id": 4,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
+    },
+    {
+        "godine": 36,
+        "id": 4,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
+    },
+    {
+        "godine": 36,
+        "id": 4,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
+    },
+    {
+        "godine": 36,
+        "id": 4,
+        "ime": "Kabir",
+        "pol": "muski",
+        "prezime": "Beba"
     }
-    document.getElementById(ink).innerHTML = " ";
-    document.getElementById(ink + 1).innerHTML = " ";
-    document.getElementById(ink + 2).innerHTML = " ";
-    document.getElementById(ink + 3).innerHTML = " ";
-    document.getElementById(ink + 4).innerHTML = " ";
-    document.getElementById(ink + 5).innerHTML = " ";
-    document.getElementById(ink + 6).innerHTML = " ";
-    document.getElementById(ink + 7).innerHTML = " ";
-    document.getElementById(ink + 8).innerHTML = " ";
+]
 
-    
+
+
+
+async function restApiGet() {
+    // const zahtev = await fetch("http://localhost:5000/get")
+    // const odgovor = await zahtev.json();
+
+    var brojElemenataJson = Object.keys(objekatJson).length;
+
+    document.getElementById("0").innerHTML = "Trenutno se nalazi " + brojElemenataJson + " elemenata u JOSN bazi.";
+
+    // ispisuje podatke iz JSON objekata pokupljenog sa servera
+    // for (const clanovi in odgovor) {
+    //     var clan = odgovor[clanovi].ime + " " + odgovor[clanovi].prezime;
+    //     document.getElementById(clanovi).innerHTML = clan;
+    // }
+
+    // ispisuje podatke iz lokalnog JSON objekta
+    for (const clanovi in objekatJson) {
+        var clan = objekatJson[clanovi].ime + " " + objekatJson[clanovi].prezime;
+        document.getElementById(clanovi).innerHTML = clan;
+    }
+
+}
+
+
+
+
+
+
+
+async function restApiPost() {
+    document.getElementById("0").innerHTML = "Ovo je post metoda."
+
+    var inputIme = document.createElement("INPUT");
+    inputIme.setAttribute("type", "text");
+    document.getElementById("mojaForma").appendChild(inputIme);
+
+    var inputPrezime = document.createElement("INPUT");
+    inputPrezime.setAttribute("type", "text");
+    document.getElementById("mojaForma").appendChild(inputPrezime);
+
+
 }
